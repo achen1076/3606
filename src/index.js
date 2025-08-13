ee08217a158965627c38d79be0435f98379a933d
achen1076 <achen1076@gmail.com>
2025-08-13 17:11:35 -0400

    analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.js";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = document.getElementById("root");
 
@@ -10,5 +11,6 @@ const app = createRoot(root);
 app.render(
   <BrowserRouter>
     <App />
+    <Analytics />
   </BrowserRouter>
 );
